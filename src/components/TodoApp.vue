fa6fa6f28d059be7686f27a7adcb3db7a425bd7c
damarowen <damarowen@gmailcom>
2022-10-16 20:11:15 +0700

    init

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="container" style="max-width: 600px">
+    <!-- Heading -->
+    <h2 class="text-center mt-5">My Vue Todo App</h2>
+
+    <!-- Input -->
+    <div class="d-flex mt-5">
+      <input
+        type="text"
+        v-model="task"
+        placeholder="Enter task"
+        class="w-100 form-control"
+      />
+      <button class="btn btn-warning rounded-0" @click="submitTask">
+        SUBMIT
+      </button>
+    </div>
+
+    <!-- Task table -->
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col" style="width: 120px">Status</th>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">#</th>
+        </tr>
+      </thead>
+      <!-- v-for is for loop in vue, :key is index  -->
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>
+            {{ task.name }}
+          </td>
+          <td>
+            {{ task.status }}
+          </td>
+          <td class="text-center">
+            <div>
+              <span class="fa fa-trash pointer"></span>
+            </div>
+          </td>
+          <td class="text-center">
+            <div>
+              <p class="fa fa-pen pointer"></p>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+      <!-- <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>
+            <span :class="{ 'line-through': task.status === 'finished' }">
+              {{ task.name }}
+            </span>
+          </td>
+          <td>
+            <span
+              class="pointer noselect"
+              @click="changeStatus(index)"
+              :class="{
+                'text-danger': task.status === 'to-do',
+                'text-success': task.status === 'finished',
+                'text-warning': task.status === 'in-progress',
+              }"
+            >
+              {{ capitalizeFirstChar(task.status) }}
+            </span>
+          </td>
+          <td class="text-center">
+            <div @click="deleteTask(index)">
+              <span class="fa fa-trash pointer"></span>
+            </div>
+          </td>
+          <td class="text-center">
+            <div @click="editTask(index)">
+              <p class="fa fa-pen pointer"></p>
+            </div>
+          </td>
+        </tr>
+      </tbody> -->
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoApp",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      // task: "",
+      // editedTask: null,
+      // statuses: ["to-do", "in-progress", "finished"],
+      /* Status could be: 'to-do' / 'in-progress' / 'finished' */
+      tasks: [
+        {
+          name: "Steal bananas from the supermarket.",
+          status: "to-do",
+        },
+        {
+          name: "Eat 1 kg chocolate in 1 hour.",
+          status: "in-progress",
+        },
+        {
+          name: "Create YouTube video.",
+          status: "finished",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
